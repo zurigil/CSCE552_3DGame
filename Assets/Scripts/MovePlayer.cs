509510d07c876071eab21b1518fb8de0ed9ff58d
Zuri <zurig@email.sc.edu>
2022-11-16 17:00:31 -0500

    Added colliders and rigid body to multiple game objects; Improved MovePlayer script to be more compatible with the physics engine so the player can collide with other objects.

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,20 +4,41 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 7.0f;
+    float inputX;
+    float inputZ;
+
+    Rigidbody rigidBody;
+    Vector3 moveVect;
+    int lockPos = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-        Vector3 moveVect = new Vector3(inputX, 0, inputY);
-        moveVect *= (moveSpeed * Time.deltaTime);
-        transform.Translate(moveVect);
+
+        //transform.rotation = Quaternion.Euler(lockPos, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        //transform.translate = Quaternion.Euler(position.x, lockPos, position.y);
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+        moveVect = new Vector3(inputX, 0, inputZ);
+        rigidBody.velocity = moveVect*moveSpeed;
     }
+
+    /*void FixedUpdate()
+    {
+        //Vector3 moveVect = new Vector3(inputX, 0, inputY);
+        //moveVect *= (moveSpeed * Time.deltaTime);
+        // transform.Translate(moveVect);
+        movePlayer(moveVect);
+    }
+    
+    void movePlayer(Vector3 direction)
+    {
+        rigidBody.velocity = (moveVect * moveSpeed);
+    }*/
 }
